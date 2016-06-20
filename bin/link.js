291a555614a62516dcd8f341a3153dc98e3c8e89
Jannick Garthen <jannick.garthen@gmail.com>
2016-06-20 21:33:50 +0200

    Remove unlink task in favor of unlink subcommand

--- bin/link.js
+++ bin/link.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 const path = require('path');
 const link = require('../lib/link');
+const unlink = require('../lib/unlink');
 
 const pkgPath = path.join(process.cwd(), 'package.json');
 const directories = require(pkgPath).requireSymlinks;
-link(directories);
+
+if (process.argv[2] === 'unlink') {
+  unlink(directories);
+} else {
+  link(directories);
+}
--- bin/unlink.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const path = require('path');
-const unlink = require('../lib/unlink');
-
-const pkgPath = path.join(process.cwd(), 'package.json');
-const directories = require(pkgPath).requireSymlinks;
-unlink(directories);
